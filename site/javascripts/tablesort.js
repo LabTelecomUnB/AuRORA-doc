922e73c8a6d317cd0f04c525300a9e6dbb36584d
ccarlaa <carlacarlaclementino@gmail.com>
2025-09-11 23:27:08 -0300

    fix initial route

---
+++ site/javascripts/tablesort.js
@@ -0,0 +1,7 @@
+// Tablesort initialization
+document$.subscribe(function() {
+  var tables = document.querySelectorAll("article table:not([class])")
+  tables.forEach(function(table) {
+    new Tablesort(table)
+  })
+})
